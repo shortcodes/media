8a4106dcfc1d81de5da1b549fd30b95230685e6b
Roman Szymański <r.szymanski@mindz.pl>
2019-07-30 15:35:46 +0200

    feat(media-transform-all): Add transactions

--- src/Commands/ProductsMediaTransform.php
+++ src/Commands/ProductsMediaTransform.php
@@ -3,6 +3,7 @@
 namespace Shortcodes\Media\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class ProductsMediaTransform extends Command
@@ -15,35 +16,65 @@ class ProductsMediaTransform extends Command
         $productModel = 'App\Models\Product\Product';
         $folderModel = 'App\Models\Import\Folder';
 
+        try {
 
-        $productModel::chunk(100, function ($objects) {
-            foreach ($objects as $object) {
+            DB::beginTransaction();
 
-                foreach ($object->media()->orderBy('position', 'asc')->get() as $media) {
-                    $storageFile = storage_path('app/' . $media->url);
-                    $object->addMedia($storageFile)->toMediaCollection('images');
+            $productModel::chunk(100, function ($objects) {
+                foreach ($objects as $object) {
+
+                    foreach ($object->medias()->orderBy('position', 'asc')->get() as $media) {
+                        $storageFile = storage_path('app/' . $media->url);
+
+                        if (file_exists($storageFile)) {
+                            $object->addMedia($storageFile)
+                                ->preservingOriginal()
+                                ->usingName(($media->name ? $media->name : $media->title) ?? 'No name')
+                                ->toMediaCollection('images');
+                        }
+                    }
+
+                    $this->info('Product ID ' . $object->id . ' done.');
+                }
+            });
+
+            DB::commit();
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+
+        try {
+            $this->info('Done all products');
+
+            $folderModel::chunk(100, function ($objects) {
+                foreach ($objects as $object) {
+
+                    foreach ($object->images()->orderBy('position', 'asc')->get() as $media) {
+                        $storageFile = storage_path('app/' . $media->url);
+
+                        if (file_exists($storageFile)) {
+                            $object->addMedia($storageFile)
+                                ->preservingOriginal()
+                                ->usingName(($media->name ? $media->name : $media->title) ?? 'No name')
+                                ->toMediaCollection('images');
+                        }
+                    }
+
+                    $this->info('Folder ID ' . $object->id . ' done.');
                 }
 
-                $this->info('Product ID ' . $object->id . ' done.');
-            }
-        });
+            });
 
-        $this->info('Done all products');
+            $this->info('Done all folders');
 
-        $folderModel::chunk(100, function ($objects) {
-            foreach ($objects as $object) {
+            DB::commit();
 
-                foreach ($object->images()->orderBy('position', 'asc')->get() as $media) {
-                    $storageFile = storage_path('app/' . $media->url);
-                    $object->addMedia($storageFile)->toMediaCollection('images');
-                }
-
-                $this->info('Folder ID ' . $object->id . ' done.');
-            }
-
-        });
-
-        $this->info('Done all folders');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
     }
 
 }
